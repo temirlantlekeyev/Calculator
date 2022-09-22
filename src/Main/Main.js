d0fd7101e2197a6c01ac3dbc57be68071e56b104
tlekeyev-t <tlekeyev.temirlan@gmail.comm>
2022-09-23 02:16:12 +0600

    second init

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -93,27 +93,35 @@ const firstlevel = [
 
 const Main = ()=> {
 
+
+    const operators = ["+", "-", "*", "/"]
+
     
     const [window, setWindow] = useState("0")
-    const [display, setDisplay] = useState()
+    const [display, setDisplay] = useState("")
 
-    const handleDisplay = () => {
-        
-        console.log("done")
-        
+    const handleDisplay = (value) => {
+        setDisplay(display + value)
+        console.log("done")    
     }
 
+    const clear = () => {
+        setDisplay("")
+    }
 
+    const calculate = () => {
+        setDisplay(eval(display).toString())
+    }
 
     return (
         <>
     
             <div className="title">IOS Calculator</div>
             <div className="wrapper">
-                <div className="window">{window}</div>
+                <div className="window">{display || "0"}</div>
                <div className="firstLevel">
 
-                    <div className="delete" onClick={handleDisplay}>
+                    <div className="delete" onClick={clear}>
                         AC
                     </div>
                     <div className="negativeValue">
@@ -122,68 +130,68 @@ const Main = ()=> {
                     <div className="percent">
                         %
                     </div>
-                    <div className="division">
+                    <div className="division" onClick={()=> handleDisplay("/")}>
                         /
                     </div>
                 </div>
                 
                 <div className="secondLevel">
 
-                    <div className="seven">
+                    <div className="seven" onClick={()=> handleDisplay("7")}>
                         7
                     </div>
-                    <div className="eight">
+                    <div className="eight" onClick={()=> handleDisplay("8")}>
                         8
                     </div>
-                    <div className="nine">
+                    <div className="nine" onClick={()=> handleDisplay("9")}>
                         9
                     </div>
-                    <div className="multiplication">
+                    <div className="multiplication" onClick={()=> handleDisplay("*")}>
                         ×
                     </div>
                 </div>
 
                 <div className="thirdLevel">
 
-                    <div className="four">
+                    <div className="four" onClick={()=> handleDisplay("4")}>
                         4
                     </div>
-                    <div className="five">
+                    <div className="five" onClick={()=> handleDisplay("5")}>
                         5
                     </div>
-                    <div className="six">
+                    <div className="six" onClick={()=> handleDisplay("6")}>
                         6
                     </div>
-                    <div className="minus">
+                    <div className="minus" onClick={()=> handleDisplay("-")}>
                         -
                     </div>
                 </div>
 
                 <div className="fourthLevel">
 
-                    <div className="one">
+                    <div className="one" onClick={()=> handleDisplay("1")}>
                         1
                     </div>
-                    <div className="two">
+                    <div className="two" onClick={()=> handleDisplay("2")}>
                         2
                     </div>
-                    <div className="three">
+                    <div className="three" onClick={()=> handleDisplay("3")}>
                         3
                     </div>
-                    <div className="plus">
+                    <div className="plus" onClick={()=> handleDisplay("+")}>
                         +
                     </div>
                 </div>
 
                 <div className="fifthLevel">
 
-                    <div className="zero">
+                    <div className="zero" onClick={()=> handleDisplay("0")}>
                         0
                     </div>
-                    <div className="comma">
+                    <div className="comma" onClick={()=> handleDisplay(",")}>
                         ,
                     </div>
-                    <div className="equal">
+                    <div className="equal" onClick={calculate}>
                         =
                     </div>
                     
@@ -196,7 +204,7 @@ const Main = ()=> {
             <div className="title">IOS Calculator</div>
             <div className="wrap">
 
-            <div className="window2">{window}</div>  
+            <div className="window2"></div>  
 
             <div value={handleDisplay} className="firstlevel" onClick={()=>handleDisplay(firstlevel.name)}>
                 {firstlevel.map((index)=> (
